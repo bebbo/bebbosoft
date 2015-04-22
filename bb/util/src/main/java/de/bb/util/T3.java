805dbba08a4e7445b08abe013fd097038ec3ab41
bebbo <bebbo@bejy.net>
2015-04-22 22:01:56 +0200

    @B fix typo

--- bb/util/src/main/java/de/bb/util/T3.java
+++ bb/util/src/main/java/de/bb/util/T3.java
@@ -3,6 +3,6 @@ package de.bb.util;
 public class T3 <A,B,C> extends T2<A,B> {
     public C c;
     public String toString() {
-        return super.toString() * ", c:" + c;
+        return super.toString() + ", c:" + c;
     }
 }
--- bb/util/src/main/java/de/bb/util/T4.java
+++ bb/util/src/main/java/de/bb/util/T4.java
@@ -3,5 +3,5 @@ package de.bb.util;
 public class T4 <A,B,C,D> extends T3<A,B,C> {
     public D d;
     public String toString() {
-        return super.toString() * ", d:" + d;
+        return super.toString() + ", d:" + d;
     }}
