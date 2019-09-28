7d36f1d1bfce8f3bc37472560219eb8ccf55924b
bebbo <bebbo@bejy.net>
2019-09-28 12:39:09 +0200

    add byte[] readFile(File file) to read the content of a file.

--- bb/io/src/main/java/de/bb/io/IOUtils.java
+++ bb/io/src/main/java/de/bb/io/IOUtils.java
@@ -13,93 +13,118 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *  
+ *
  *****************************************************************************/
 package de.bb.io;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 /**
  * Some useful methods to read or copy date via streams.
- * 
+ *
  * @author bebbo
  *
  */
 public class IOUtils {
 
-    /**
-     * Copy the given length of data from the given <code>InputStream</code> to
-     * the given <code>OutputStream</code>.
-     * 
-     * @param is
-     *            the <code>InputStream</code>
-     * @param os
-     *            the <code>OutputStream</code>
-     * @param length
-     *            the length of data to copy.
-     * @throws IOException
-     *             on error, e.g. if the given length of data was not reached.
-     */
-    public static void copy(InputStream is, OutputStream os, long length)
-            throws IOException {
-        byte b[] = new byte[8192];
-        while (length > 0) {
-            int b0 = is.read();
-            if (b0 < 0)
-                throw new IOException("EOS");
-            os.write(b0);
-            --length;
-            if (length == 0)
-                return;
-            int read = length > b.length ? b.length : (int) length;
-            read = is.read(b, 0, read);
-            if (read > 0) {
-                os.write(b, 0, read);
-                length -= read;
-            }
-        }
-    }
+	/**
+	 * Copy the given length of data from the given <code>InputStream</code> to the
+	 * given <code>OutputStream</code>.
+	 * 
+	 * @param is
+	 *            the <code>InputStream</code>
+	 * @param os
+	 *            the <code>OutputStream</code>
+	 * @param length
+	 *            the length of data to copy.
+	 * @throws IOException
+	 *             on error, e.g. if the given length of data was not reached.
+	 */
+	public static void copy(InputStream is, OutputStream os, long length) throws IOException {
+		byte b[] = new byte[8192];
+		while (length > 0) {
+			int b0 = is.read();
+			if (b0 < 0)
+				throw new IOException("EOS");
+			os.write(b0);
+			--length;
+			if (length == 0)
+				return;
+			int read = length > b.length ? b.length : (int) length;
+			read = is.read(b, 0, read);
+			if (read > 0) {
+				os.write(b, 0, read);
+				length -= read;
+			}
+		}
+	}
 
-    /**
-     * Read the given length of data from the given <code>InputStream</code> to
-     * the given <code>byte []</code> buffer.
-     * 
-     * @param is
-     *            the <code>InputStream</code>
-     * @param buffer
-     *            the <code>byte []</code> buffer
-     * @param position
-     *            the offset into the buffer to read to
-     * @param length
-     *            the length of data to copy.
-     * @throws IOException
-     *             on error, e.g. if the given length of data was not reached.
-     */
-    public static void readFully(InputStream is, byte[] buffer, int position,
-            int length) throws IOException {
-        while (position < length) {
-            int b0 = is.read();
-            if (b0 < 0)
-                throw new IOException("EOS");
-            buffer[position++] = (byte) b0;
-            position += is.read(buffer, position, length - position);
-        }
-    }
+	/**
+	 * Read the given length of data from the given <code>InputStream</code> to the
+	 * given <code>byte []</code> buffer.
+	 * 
+	 * @param is
+	 *            the <code>InputStream</code>
+	 * @param buffer
+	 *            the <code>byte []</code> buffer
+	 * @param position
+	 *            the offset into the buffer to read to
+	 * @param length
+	 *            the length of data to copy.
+	 * @throws IOException
+	 *             on error, e.g. if the given length of data was not reached.
+	 */
+	public static void readFully(InputStream is, byte[] buffer, int position, int length)
+			throws IOException {
+		while (position < length) {
+			int b0 = is.read();
+			if (b0 < 0)
+				throw new IOException("EOS");
+			buffer[position++] = (byte) b0;
+			position += is.read(buffer, position, length - position);
+		}
+	}
 
-    /**
-     * Read the data from the given <code>InputStream</code> to the given
-     * <code>byte []</code> buffer and fill starting at 0.
-     * 
-     * @param is
-     *            the <code>InputStream</code>
-     * @param buffer
-     *            the <code>byte []</code> buffer
-     * @throws IOException
-     *             on error, e.g. if the given length of data was not reached.
-     */
-    public static void readFully(InputStream is, byte[] buffer) throws IOException {
-        readFully(is, buffer, 0, buffer.length);
-    }
+	/**
+	 * Read the data from the given <code>InputStream</code> to the given
+	 * <code>byte []</code> buffer and fill starting at 0.
+	 * 
+	 * @param is
+	 *            the <code>InputStream</code>
+	 * @param buffer
+	 *            the <code>byte []</code> buffer
+	 * @throws IOException
+	 *             on error, e.g. if the given length of data was not reached.
+	 */
+	public static void readFully(InputStream is, byte[] buffer) throws IOException {
+		readFully(is, buffer, 0, buffer.length);
+	}
+
+	/**
+	 * Read a complete file into byte array.
+	 * 
+	 * @param file
+	 *            the file
+	 * @return the byte array.
+	 * @throws IOException
+	 */
+	public static byte[] readFile(File file) throws IOException {
+		long llen = file.length();
+		if (llen > Integer.MAX_VALUE)
+			throw new IOException("file " + file.getAbsolutePath() + " is too large");
+
+		int len = (int) llen;
+		byte b[] = new byte[len];
+		FileInputStream fis = new FileInputStream(file);
+		try {
+			readFully(fis, b);
+			return b;
+		} finally {
+			fis.close();
+		}
+	}
 }
